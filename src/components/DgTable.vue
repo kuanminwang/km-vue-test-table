aa70e032be7bd9fdad108f12634dad8f42cd7cf1
kuanminwang <38400596+kuanminwang@users.noreply.github.com>
2020-03-14 22:24:05 +0100

    table 1st version

---
+++ src/components/DgTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="column in columns" :key="column">{{ column }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, idx) in datas" :key="idx">
+          <td v-for="column in columns" :key="column">
+            <slot :name="column" :value="data"></slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array,
+      required: true
+    },
+    datas: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      status: "critical"
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = "normal";
+    }
+  }
+};
+</script>
+
+<style>
+  table{
+    border: 1px solid black;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <dg-table :columns="columns" :datas="users">
+      <template slot="firstName" slot-scope="data">{{ data.value['firstName'] }}</template>
+      <template slot="lastName"  slot-scope="data">{{ data.value['lastName'] }}</template>
+      <template slot="fullName"  slot-scope="data">{{ data.value['fullName'] }}</template>
+    </dg-table>
+  </div>
+</template>
+
+<script>
+import DgTable from "./DgTable";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    "dg-table": DgTable
+  },
+  data() {
+    return {
+      users:[
+        {firstName: 'km', lastName: 'wang', fullName: 'km wang'},
+        {firstName: 'jeff', lastName: 'liao', fullName: 'jeff wang'},
+      ],
+      columns:['firstName', 'lastName', 'fullName']
+    };
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  font-weight: normal;
+}
+</style>
